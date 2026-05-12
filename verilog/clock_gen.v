425c30db1f4e37fe19ddca373ee201bc8448637c
DeepAung <i.deepaung@gmail.com>
2026-05-12 21:12:48 +0700

    fix: fix clock_gen

--- verilog/clock_gen.v
+++ verilog/clock_gen.v
@@ -2,22 +2,24 @@ module clock_gen (
     input  wire clk_100MHz,
     input  wire reset,
     output wire clk_25MHz,
-    output reg  locked
+    output wire locked
 );
 `ifdef __ICARUS__
     reg [1:0] counter = 2'b0;
 
+    reg locked_reg;
     always @(posedge clk_100MHz) begin
         if (reset) begin
             counter <= 0;
-            locked  <= 0;
+            locked_reg  <= 0;
         end else begin
             counter <= counter + 1;
-            locked  <= 1;
+            locked_reg <= 1;
         end
     end
 
     assign clk_25MHz = counter[1];
+    assign locked = locked_reg;
 `else
     clk_wiz_0 clk_wiz (
         .clk_in1 (clk_100MHz),
