9cdb870ef391aaf5bb32a06425edb6ed7e701a9b
DeepAung <i.deepaung@gmail.com>
2026-05-12 20:36:57 +0700

    feat: update clock gen

--- verilog/clock_gen.v
+++ verilog/clock_gen.v
@@ -1,17 +1,29 @@
 module clock_gen (
-    input  clk_100MHz,
-    input  reset,
-    output clk_50MHz,
-    output clk_25MHz
+    input  wire clk_100MHz,
+    input  wire reset,
+    output wire clk_25MHz,
+    output reg  locked
 );
-    reg [1:0] counter;
+`ifdef __ICARUS__
+    reg [1:0] counter = 2'b0;
 
     always @(posedge clk_100MHz) begin
-        if (reset) counter <= 0;
-        else counter <= counter + 1;
+        if (reset) begin
+            counter <= 0;
+            locked  <= 0;
+        end else begin
+            counter <= counter + 1;
+            locked  <= 1;
+        end
     end
 
-    assign clk_25MHz = (counter == 0) ? 1 : 0;
-    assign clk_50MHz = (counter == 0 || counter == 2) ? 1 : 0;
-
+    assign clk_25MHz = counter[1];
+`else
+    clk_wiz_0 clk_wiz (
+        .clk_in1 (clk_100MHz),
+        .reset   (reset),
+        .clk_out1(clk_25MHz),
+        .locked  (locked)
+    );
+`endif
 endmodule
--- verilog/top.v
+++ verilog/top.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module top (
-    input clk_100MHz,
-    input reset,
-    input [1:0] sw,           // NEW: 2 slide switches for filter selection
+    input       clk_100MHz,
+    input       btnC,
+    input [1:0] sw,
 
     output        vga_hsync,
     output        vga_vsync,
@@ -20,21 +20,15 @@ module top (
     output       OV7670_XCLK
 );
 
-    // Clock generator
-    wire clk_50MHz;
+    // Clock Generator
     wire clk_25MHz;
+    wire clk_25MHz_locked;
+    wire reset = btnC || !clk_25MHz_locked;
     clock_gen clock_gen_inst (
         .clk_100MHz(clk_100MHz),
-        .clk_50MHz (clk_50MHz),
-        .clk_25MHz (clk_25MHz)
-    );
-
-    // Reset btn debouncer
-    wire reset_debounced;
-    debounce debounce_inst (
-        .clk(clk_50MHz),
-        .i  (reset),
-        .o  (reset_debounced)
+        .reset     (btnC),
+        .clk_25MHz (clk_25MHz),
+        .locked    (clk_25MHz_locked)
     );
 
     // ---------------------- //
@@ -45,12 +39,12 @@ module top (
     wire        capture_we;  // Write Enable from camera
 
     wire [16:0] display_addr;
-    wire [11:0] frame_pixel; // Data read from RAM
+    wire [11:0] frame_pixel;  // Data read from RAM
 
     // Camera Configuration Hardware Pins
-    assign OV7670_PWDN = 1'b0; // Power down: Normal mode
-    assign OV7670_RST  = 1'b1; // Reset: Active low, so keep high
-    assign OV7670_XCLK = clk_25MHz; // Camera needs a system clock
+    assign OV7670_PWDN = 1'b0;  // Power down: Normal mode
+    assign OV7670_RST  = 1'b1;  // Reset: Active low, so keep high
+    assign OV7670_XCLK = clk_25MHz;  // Camera needs a system clock
 
     camera_capture capture (
         .pclk (OV7670_PCLK),
@@ -74,7 +68,7 @@ module top (
 
     I2C_AV_Config IIC (
         .iCLK       (clk_25MHz),
-        .iRST_N     (!reset_debounced),
+        .iRST_N     (!reset),
         .Config_Done(),
         .I2C_SDAT   (OV7670_SDAT),
         .I2C_SCLK   (OV7670_SCLK),
@@ -85,16 +79,17 @@ module top (
     // ---------------------- //
 
     vga_display vga_display (
-        .clk25     (clk_25MHz),
-        .filter_sw (sw),               // Connect the switches to the display module
-        .vga_red   (vga_rgb[11:8]),
-        .vga_green (vga_rgb[7:4]),
-        .vga_blue  (vga_rgb[3:0]),
-        .vga_hsync (vga_hsync),
-        .vga_vsync (vga_vsync),
+        .clk25    (clk_25MHz),
+        .filter_sw(sw),
+        .vga_red  (vga_rgb[11:8]),
+        .vga_green(vga_rgb[7:4]),
+        .vga_blue (vga_rgb[3:0]),
+        .vga_hsync(vga_hsync),
+        .vga_vsync(vga_vsync),
 
-        .frame_addr (display_addr),    // FIXED: Connected properly to BRAM read address
+        .frame_addr (display_addr),
         .frame_pixel(frame_pixel)
     );
 
 endmodule
+
